8539e104ea5d26a3ed95647150ddca8b3df61c95
thedammyking <kingdammie@gmail.com>
2019-02-08 22:52:56 +0100

    update

--- src/routes/auth/forgot_password.js
+++ src/routes/auth/forgot_password.js
@@ -1,13 +1,11 @@
 import { p_two_req } from "../../config/agent";
-import Mailgun from "mailgun-js";
 import { mailgun_api_key, mailgun_domain } from "../../config";
+const mailgun = require("mailgun-js")({
+  apiKey: mailgun_api_key,
+  domain: mailgun_domain
+});
 
 export default async function(req, res) {
-  const mailgun = new Mailgun({
-    apiKey: mailgun_api_key,
-    domain: mailgun_domain
-  });
-
   try {
     const data = await p_two_req(
       "checkemail",
