6348011b49dd9f233fb89544df9054c30ffefb3d
thedammyking <kingdammie@gmail.com>
2019-02-09 05:46:13 +0100

    update

--- src/routes/auth/register.js
+++ src/routes/auth/register.js
@@ -8,6 +8,25 @@ export default async function(req, res) {
     users_password: req.body.password
   };
   try {
+    const check_one = await p_one_req(
+      "checkemail",
+      "post",
+      JSON.stringify({ email: req.body.email })
+    ).then(res => res.json());
+
+    const check_two = await p_two_req(
+      "checkemail",
+      "post",
+      JSON.stringify({ email: req.body.email })
+    ).then(res => res.json());
+
+    if (check_one.status || check_two.status) {
+      return res.status(200).json({
+        status: false,
+        message: `User with email: ${req.body.email} aleady exist`
+      });
+    }
+
     const res_one = await p_one_req(
       "register",
       "post",
@@ -20,7 +39,7 @@ export default async function(req, res) {
       JSON.stringify(req.body)
     ).then(res => res.json());
 
-    res.status(200).json(res_two);
+    return res.status(200).json(res_two);
   } catch (error) {
     console.log(error);
   }
